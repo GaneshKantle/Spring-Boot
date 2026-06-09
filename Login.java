999d546ef5b88fd9453851bd75b66a3662ee9b8a
PunithB1601 <punithb072@gmail.com>
2026-06-09 10:59:11 +0530

    file modified, added few variables

--- Login.java
+++ Login.java
@@ -1,5 +1,8 @@
 class Login {
     public static void main(String[] args) {
         System.out.println("Hello World");
+        int a=10;
+        int b=10;
+        System.out.println(a+b);
     }
 }
